b1d43c4644e83548f695a46594181b8ff063b4f0
Mendez <alexis.mendezpvd@gmail.com>
2019-09-09 14:03:31 -0400

    added tabbed content

--- app.js
+++ app.js
@@ -57,3 +57,21 @@ searchBar.addEventListener('keyup', function(e){
       book.style.display ='none'; 
     }
 });
+}); 
+
+// tabbed content  
+const tabs = document.querySelector('.tabs'); 
+const panels = document.querySelectorAll ('.panel');
+tabs.addEventListener('click', (e) => {
+  if(e.target.tagName == 'LI'){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    Array.from(panels).forEach((panel) => {
+      if(panel == targetPanel){
+        panel.classList.add('active');
+      }else{
+        panel.classList.remove('active');
+      }
+    });
+  }
+});
+
